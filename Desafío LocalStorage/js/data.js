9f1e00d1860e1ae81b0dab4449a5ec43603c51f8
masteraised <137823193+masteraised@users.noreply.github.com>
2023-08-21 15:22:06 -0300

    Delete Desafío LocalStorage directory

--- Desafío LocalStorage/js/data.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const datoAlmacenado = localStorage.getItem("dato");
-    if (datoAlmacenado) {
-        const dataElement = document.getElementById("data");
-        dataElement.textContent = datoAlmacenado;
-    }
-});
--- Desafío LocalStorage/js/index.js
+++
@@ -1,8 +0,0 @@
-const inputText = document.getElementById('inputText');
-const buttonText = document.getElementById('buttonText');
-
-
-buttonText.addEventListener("click", function() {
-    const datoIngresado = inputText.value;
-    localStorage.setItem("dato", datoIngresado);
-});
